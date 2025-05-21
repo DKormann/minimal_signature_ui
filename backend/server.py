634ff838af6c0c92e66c79171bbd4b0fef4cecd5
DKormann <dominikormann@outlook.com>
2025-05-21 15:18:46 +0200

    fix backend

--- backend/server.py
+++ backend/server.py
@@ -6,14 +6,13 @@ from flask_cors import CORS
 app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
 CORS(app)
 
-# @app.route('/api/hello', methods=['GET'])
-# def hello():
-#   return jsonify({'message': 'Hello, World!'})
+@app.route('/api/hello', methods=['GET'])
+def hello():
+  return jsonify({'message': 'Hello, World!'})
 
 @app.route('/api/message', methods=['GET'])
-def hello():
-  return jsonify({'message': 'this is a server message!'})
-
+def message():
+  return jsonify({'message': 'this is not a server message!'})
 
 if __name__ == '__main__':
   app.run(debug=True)
