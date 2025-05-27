0e8214ab4ff93a1f0adb770448fdd6acfe1537de
DKormann <dominikormann@outlook.com>
2025-05-27 22:48:53 +0200

    backend

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
 CORS(app)
@@ -13,6 +14,14 @@ def hello():
 @app.route('/api/message', methods=['GET'])
 def message():
   return jsonify({'message': 'this is not a server message!'})
+
+@app.route('/api/list_documents', methods=['GET'])
+def list_documents():
+  return jsonify(os.listdir('./documents'))
+
+@app.route("/", methods = ["GET"])
+def index(): return app.send_static_file('index.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
