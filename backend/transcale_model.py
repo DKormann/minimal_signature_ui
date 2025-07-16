7fff58b35ae9d2ed4b01c1336a3ba146758c05cf
dkormann <dominikormann@outlook.com>
2025-07-16 15:44:56 +0200

    clean

--- backend/transcale_model.py
+++ backend/transcale_model.py
@@ -21,9 +21,7 @@ template = Image.open(Path("../template.png"))
 display(template)
 
 
-#%%
 
-np.concat
 
 #%%
 
@@ -105,11 +103,17 @@ def apply_affine(matrix, keypoints):
   transformed = hom_kps @ matrix.transpose(1, 2)  # -> (N, 2)
   return transformed
 
-def affine_loss(P, Y , width, height):
+def affine_loss(P, Y , width, height, plot=False):
   points = torch.tensor([[0,0],[0,1],[1,0],[1,1],], dtype=torch.float32)*torch.tensor([width, height])
 
+  print(points.shape)
   pred = apply_affine(P, points)
   label = apply_affine(Y, points)
+  if plot:
+    plt.scatter(label.detach()[0,:,0], label.detach()[0,:,1])
+    plt.scatter(pred[0,:,0], pred[0,:,1])
+
+
   return (pred-label).square().sum(-1).mean()/torch.tensor([width, height]).square().sum()
 
 def show_points(points):
@@ -121,155 +125,115 @@ width, height = template.size
 mat = create_affine(torch.tensor([.1]), torch.tensor(1.), torch.tensor([(0,0)]))
 mat.requires_grad = True
 
-
-# opt = torch.optim.Adam([mat], lr=0.005)
-
-opt = torch.optim.SGD([mat], lr=0.005)
-
-
-#%%
+opt = torch.optim.SGD([mat], lr=0.01)
 
 Y = create_affine(torch.tensor([0.3]), torch.tensor(1.), torch.tensor([(0, 0)]))
 
-loss = affine_loss(
-Y,
-mat,
-width,height)
+#%%
+
+opt.zero_grad()
+loss = affine_loss(Y,mat,width,height, 0)
 loss.backward()
 
 opt.step()
-
-
-show_points(apply_affine(Y, points))
-show_points(apply_affine(mat, points))
 print(loss.detach().numpy())
 
+#%%
+
+
+affine_loss(Y, mat, width, height, 1)
 
 
 
 #%%
-
-# #%%
-
-# mat = transformation.new(1, -0.1, (-100,0)).mat
-
-# mat = np.concat((mat, np.array([[0, 0, 1]])), axis=0)
-
-
-# # np.linalg.inv(mat)[:2,:]
-
 
 transformed =  template.transform(template.size, PIL.ImageTransform.AffineTransform((
   1.5, 0.5, -100,
   -.5, 1.5, 50
-  
   )) )
 transformed = Image.alpha_composite(Image.new("RGBA", transformed.size, "white"), transformed.convert("RGBA"))
 display(transformed)
-# #%%
 
 
-# transformed =  template.transform(template.size, PIL.ImageTransform.AffineTransform((
-#   1.5, 0.5, -100,
-#   -.5, 1.5, 50
-#   )) )
-# transformed = Image.alpha_composite(Image.new("RGBA", transformed.size, "white"), transformed.convert("RGBA"))
-# display(transformed)
+#%%
+
+class BasicBlock(nn.Module):
+  def __init__(self, in_c, out_c, stride=1):
+    super().__init__()
+    self.conv1 = nn.Conv2d(in_c, out_c, 3, stride, padding=1, bias=False)
+    self.bn1 = nn.BatchNorm2d(out_c)
+    self.conv2 = nn.Conv2d(out_c, out_c, 3, 1, padding=1, bias=False)
+    self.bn2 = nn.BatchNorm2d(out_c)
+
+    self.shortcut = nn.Sequential()
+    if stride != 1 or in_c != out_c:
+      self.shortcut = nn.Sequential(
+        nn.Conv2d(in_c, out_c, 1, stride, bias=False),
+        nn.BatchNorm2d(out_c)
+      )
+
+  def forward(self, x):
+    out = nn.functional.relu(self.bn1(self.conv1(x)))
+    out = self.bn2(self.conv2(out))
+    out += self.shortcut(x)
+    return nn.functional.relu(out)
 
 
+class SmallResNet(nn.Module):
+  def __init__(self, num_classes=6):
+    super().__init__()
+    self.conv = nn.Conv2d(3, 64, 3, stride=1, padding=1, bias=False)
+    self.bn = nn.BatchNorm2d(64)
 
-# #%%
+    specs = [
+      (64, 64, 2, 2),
+      (64, 100, 2, 4),
+      (100, 128, 2, 4),
+    ]
 
-
-
-# #%%
-
-
-# def create_XY(k = 100):
-#   """k is the size of the training snippets"""
-
-#   t_k = min(template_arr.size)
-  
-#   max_scale = max(0.6, t_k / k)
-#   scale = 
-  
-#   resized = template.resize(())
-
-# create_XY()
-
-# #%%
-
-# template_arr.shape # (848, 1361, 3)
-
-# #%%
-
-
-# class BasicBlock(nn.Module):
-#   def __init__(self, in_c, out_c, stride=1):
-#     super().__init__()
-#     self.conv1 = nn.Conv2d(in_c, out_c, 3, stride, padding=1, bias=False)
-#     self.bn1 = nn.BatchNorm2d(out_c)
-#     self.conv2 = nn.Conv2d(out_c, out_c, 3, 1, padding=1, bias=False)
-#     self.bn2 = nn.BatchNorm2d(out_c)
-
-#     self.shortcut = nn.Sequential()
-#     if stride != 1 or in_c != out_c:
-#       self.shortcut = nn.Sequential(
-#         nn.Conv2d(in_c, out_c, 1, stride, bias=False),
-#         nn.BatchNorm2d(out_c)
-#       )
-
-#   def forward(self, x):
-#     out = nn.functional.relu(self.bn1(self.conv1(x)))
-#     out = self.bn2(self.conv2(out))
-#     out += self.shortcut(x)
-#     return nn.functional.relu(out)
-
-
-# class SmallResNet(nn.Module):
-#   def __init__(self, num_classes=3):
-#     super().__init__()
-#     self.conv = nn.Conv2d(3, 64, 3, stride=1, padding=1, bias=False)
-#     self.bn = nn.BatchNorm2d(64)
+    def make_layer(in_c, out_c, num_blocks, stride):
+      layers = [BasicBlock(in_c, out_c, stride)]
+      for _ in range(1, num_blocks): layers.append(BasicBlock(out_c, out_c))
+      return nn.Sequential(*layers)
     
-#     self.layer1 = self._make_layer(64, 64, num_blocks=2, stride=2)
-#     self.layer2 = self._make_layer(64, 128, num_blocks=2, stride=4)
-#     # self.layer3 = self._make_layer(128, 256, num_blocks=2, stride=2)
-
-#     self.pool = nn.AdaptiveAvgPool2d(1)
-#     self.fc = nn.Linear(128, num_classes)
-
-#   def _make_layer(self, in_c, out_c, num_blocks, stride):
-#     layers = [BasicBlock(in_c, out_c, stride)]
-#     for _ in range(1, num_blocks):
-#         layers.append(BasicBlock(out_c, out_c))
-#     return nn.Sequential(*layers)
-
-#   def forward(self, x):
-#     out = nn.functional.relu(self.bn(self.conv(x)))
-#     out = self.layer1(out)
-#     out = self.layer2(out)
-#     # out = self.layer3(out)
-#     out = self.pool(out)
-#     out = out.view(out.size(0), -1)
-#     return self.fc(out)
+    self.layers = nn.ModuleList(list(map(lambda spec: make_layer(*spec), specs)))
+    self.pool = nn.AdaptiveAvgPool2d(1)
+    self.fc = nn.Linear(specs[-1][1], num_classes)
 
 
-# model = SmallResNet()
+  def forward(self, x):
+    out = nn.functional.relu(self.bn(self.conv(x)))
+    for layer in self.layers: out = layer(out)
+    out = self.pool(out)
+    out = out.view(out.size(0), -1)
+    return self.fc(out)
 
-# print(sum(p.numel() for p in model.parameters()))
+model = SmallResNet()
+model = torch.compile(model)
 
-# x = torch.tensor(template_arr, dtype=torch.float32).permute( 2, 0, 1).unsqueeze(0) / 255.0
-# print(x.shape)
+#%%
 
-# #%%
+def step ():
 
-# plt.imshow(x[0].permute(1,2,0)[200:400,200:400].detach().numpy())
+  rot = torch.rand(1).item() - .5
+  tran = transformation.new(1., rot, (0, 0))
+  img = tran.apply_image(template)
+  template_arr = torch.tensor(np.array(img)[:,:,:3])
 
-# #%%
+  print(template_arr.shape)
+
+  template_arr = torch.stack([template_arr, template_arr, template_arr], dim=0)
+  print(template_arr.shape)
+  
+  x = (template_arr).permute(0, 3, 1, 2) / 255.0
 
 
-# pred = model(x[:,:,:1000,:1000])
-# pred.shape
+  print(x.shape,x.dtype)
 
-# #%%
+  pred=model(x)
+  return pred.shape
+
+
+step()
+
+#%%
